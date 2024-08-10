8567aaa4fd21e186438080cb60304d0522ae840c
Web MQM <mqmusa090@gmail.com>
2024-08-09 21:13:53 -0400

    Fix?

--- src/routes/political_struggle/+page.svelte
+++ src/routes/political_struggle/+page.svelte
@@ -1,51 +1,57 @@
-<div class="max-w-4xl mx-auto p-4 mb-8">
-    <h2 class="text-3xl font-bold">Founder and Leader</h2>
-    <p>Altaf Hussain Bhai started his political struggle on 11 June, 1978 by founding the All Pakistan Mohajir Students Organization.</p>
-  
-    <hr class="my-4 border-gray-300">
-  
-    <h2 class="text-3xl font-bold">Early Life</h2>
-    <p>He was born on September 17, 1953 at Karachi. His parents Nazir Hussain and Khurshid Begum both belonged to Agra and migrated to Pakistan after partition.</p>
-  
+<div>
+    <h2 class="text-3xl font-bold">Altaf Hussain Bhai</h2>
+    
+    <p>Founder and leader Altaf Hussain Bhai started his political struggle on 11 June, 1978 by founding the All Pakistan Mohajir Students Organization.</p>
+
     <div class="mb-4">
-      <h3 class="text-lg font-bold">Education</h3>
-      <ul>
-        <li>Matric in 1969 from Government Boys Secondary School Jail Road, Karachi.</li>
-        <li>F.Sc. from Government City College, Karachi.</li>
-        <li>B.Sc from Islamia Science College in 1974.</li>
-        <li>Secured a degree in B. Pharmacy from Karachi University in 1979.</li>
-      </ul>
+        <h3 class="text-lg font-bold">Early Life</h3>
+        <ul>
+            <li>He was born on September 17, 1953 at Karachi.</li>
+            <li>His parents Nazir Hussain and Khurshid Begum both belonged to Agra and migrated to Pakistan after partition.</li>
+        </ul>
     </div>
-  
+
     <hr class="my-4 border-gray-300">
-  
-    <h2 class="text-3xl font-bold">Politics</h2>
+
+    <h2 class="text-3xl font-bold">Education</h2>
+    <p>Altaf Hussain did his matric in 1969 form Government Boys Secondary School Jail Road, Karachi. After passing his F.Sc. from Government City College, he joined the National Cadet Service Scheme but could not complete his training with the 57 Bloch regiment due to an unpleasant incident and returned again to pursue his studies.</p>
+
+    <div class="mb-4">
+        <h3 class="text-lg font-bold">Education Continuation</h3>
+        <ul>
+            <li>He did his B.Sc from Islamia Science College in 1974.</li>
+            <li>Secured a degree in B. Pharmacy from Karachi University in 1979 but due to uncertain conditions he had to leave the university.</li>
+        </ul>
+    </div>
+
+    <hr class="my-4 border-gray-300">
+
+    <h2 class="text-3xl font-bold">Political Career</h2>
     <p>Altaf Hussain became active in politics during his university days and founded the All Pakistan Muhajir Students Organization, (APMSO). This organization was later upgraded to Mohajir Qaumi Movement (MQM) on March 18, 1984.</p>
-  
+
     <div class="mb-4">
-      <h3 class="text-lg font-bold">Objectives</h3>
-      <ul>
-        <li>Urge the government to accept Mohajirs as a nationality of Pakistan like any other nationality of the country.</li>
-        <li>Right of representation and grant of quota in all educational institutions and jobs throughout the Sind province.</li>
-        <li>Quota in power echelon too.</li>
-      </ul>
+        <h3 class="text-lg font-bold">Objective of MQM</h3>
+        <ul>
+            <li>The main objective of the Mohajir Quami Movement was to urge the government to accept Mohajirs as a nationality of Pakistan like any other nationality of the country.</li>
+            <li>In the center they demanded the right of representation and grant of quota in all educational institutions and jobs throughout the Sind province.</li>
+        </ul>
     </div>
-  
+
     <hr class="my-4 border-gray-300">
-  
+
     <h2 class="text-3xl font-bold">Exile</h2>
     <p>After he was severely criticized by the intellectuals and the general public, he was obliged to make his MQM onwards as Muttahida Quami Movement.</p>
-  
+
     <div class="mb-4">
-      <h3 class="text-lg font-bold">London Exile</h3>
-      <ul>
-        <li>He has been living in exile since then and continues to play his role as a leading his party from London.</li>
-        <li>He addresses the public meetings and rallies from time to time over phone / internet.</li>
-      </ul>
+        <h3 class="text-lg font-bold">London Exile</h3>
+        <ul>
+            <li>He has been living in exile since then and continues to play his role as a leading his party from London.</li>
+            <li>He addresses the public meetings and rallies from time to time over phone / internet.</li>
+        </ul>
     </div>
-  
+
     <hr class="my-4 border-gray-300">
-  
+
     <h2 class="text-3xl font-bold">Recent Events</h2>
     <p>In 2013 army start a new operation against MQM. They ban on Speach of Altaf Hussain in Pakistan, Closed all offices of MQM and burned the Altaf Hussain House 90.</p>
 </div>
