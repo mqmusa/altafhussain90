784bf6a94adb42721cd123203c8e2a616fc112dc
Web MQM <mqmusa090@gmail.com>
2024-08-09 22:38:06 -0400

    Fixed Formatting of the first line

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -119,134 +119,8 @@
     background-repeat: no-repeat;
     background-position: center;
 }
-
-/* .blink {
-  animation: blink-animation 8s steps(8, start) infinite;
-  -webkit-animation: blink-animation 8s steps(8, start) infinite;
-}
-
-@keyframes blink-animation {
-  50% {
-    visibility: hidden;
-  }
-  51% {
-    visibilty: visible;
-  }
-}
-
-@-webkit-keyframes blink-animation {
-  50% {
-    visibility: hidden;
-  }
-  51% {
-    visibilty: visible;
-  }
-} */
-
-/* .blink {
-  animation: blink-animation 2s steps(10, start) infinite;
-  -webkit-animation: blink-animation 12s steps(100, start) infinite;
-}
-
-@keyframes blink-animation {
-  0% {
-    visibility: visible;
-  }
-  40% {
-    visibility: hidden;
-  }
-  50% {
-    visibility: hidden;
-  }
-  100% {
-    visibility: visible;
-  }
-} */
-
-
-/* @-webkit-keyframes blink-animation {
-  0% {
-    visibility: visible;
-  }
-  47% {
-    visibility: hidden;
-  }
-  48% {
-    visibility: visible;
-  }
-  50% {
-    visibility: hidden;
-  }
-  51% {
-    visibility: visible;
-  }
-} */
 </style>
 
-{#if isCustom}
-
-<!-- First, we need to introduce him -->
-<!--
-<section class="bg-gradient-to-tr from-green-600 to-gray-300 to-60%">
-    <div class="bg-contain bg-url- bg-no-repeat bg-center md:bg-none max-w-screen-xl px-4 py-8 mx-auto md:gap-8 md:gap-0 md:py-16 md:grid md:grid-cols-12">
-        <div class="place-self-center text-center md:text-left md:mr-auto md:col-span-7">
-            <h1 class="max-w-2xl mb-3 text-3xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl dark:text-white">
-                Help This Generation
-            </h1>
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-green-600">
-                Build a Better Nation
-            </h1>
-        </div>   
-        <div class="hidden mt-0 z-10 md:block md:col-span-5">
-            <img src={speech} alt="Altaf Hussain Giving a Speech"> 
-        </div>     
-    </div>
-</section>
--->
-<!-- Second, short bio -->
-<!--
-<section class="bg-gradient-to-tr from-green-600 to-gray-300 to-60%">
-    <div class="max-w-screen-xl px-4 py-8 mx-auto md:gap-8 md:gap-0 md:py-16 md:grid md:grid-cols-12">
-        <div class="place-self-center text-center md:text-left md:mr-auto md:col-span-7">
-            <h1 class="max-w-2xl mb-3 text-3xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl dark:text-white">
-                
-            </h1>
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-green-600">
-                Build a Better Nation
-            </h1>
-        </div>   
-        <div class="hidden mt-0 z-10 md:block md:col-span-5">
-            <img src={speech} alt="Altaf Hussain Giving a Speech"> 
-        </div>     
-    </div>
-</section>
--->
-
-{:else}
-<!--
-<section class="bg-gradient-to-tr from-green-900">
-    <div class="bg-contain bg-url bg-no-repeat bg-center md:bg-none max-w-screen-xl px-4 py-8 mx-auto md:gap-8 md:gap-0 md:py-16 md:grid md:grid-cols-12">
-        <div class="place-self-center text-center md:text-left md:mr-auto md:col-span-7">
-            <h1 class="max-w-2xl mb-3 text-3xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl dark:text-white">
-                Help This Generation
-            </h1>
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-green-600">
-                Build a Better Nation
-            </h1>
-            <p class="max-w-2xl mb-3 text-2xl tracking-tight leading-none md:text-3xl xl:text-4xl dark:text-white">
-                Join Altaf Hussain Today
-            </p>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                Donate Now
-                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-        </div>   
-        <div class="hidden mt-0 z-10 md:block md:col-span-5">
-            <img src={pose} alt="Altaf Hussain Giving a Speech"> 
-        </div>     
-    </div>
-</section>
--->
 <section id="hero" class="bg-gradient-to-br from-secondary-600">
     {#if heroState == 0}
         <div    in:slide={{ delay: 1000, duration: animSpeed, axis: 'x' }}
@@ -277,7 +151,10 @@
     <div class="max-w-screen-xl mx-auto py-8 md:py-16">
         <div class="flex flex-col place-self-center items-center text-center">
             <h1 class="max-w-2xl mb-5 text-3xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl text-primary-600 dark:text-green-300">
-                Thank you for Gifting to Your Loving Quaid
+                Thank you for Gifting
+            </h1>
+            <h1 class="max-w-2xl mb-5 text-3xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl text-primary-600 dark:text-green-300">
+                To Your Loving Quaid
             </h1>
             
 
